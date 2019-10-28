664a62bbf67216651a1b02023ab37917380b37d3
Sara Lissette <lissette.ibnz@gmail.com>
2019-10-28 17:02:50 +0000

    Pass store to Vue instance

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import store from "./store";
 import vuetify from "./core/plugins/vuetify";
 
 Vue.use(vuetify);
@@ -7,6 +8,7 @@ Vue.use(vuetify);
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   vuetify,
   render: h => h(App),
 }).$mount("#app");
