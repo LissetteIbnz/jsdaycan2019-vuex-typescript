cd208f85c5aef7e555b3cf9fa7f2773b73a6292e
Sara Lissette <lissette.ibnz@gmail.com>
2019-10-28 17:03:44 +0000

    Update mutations with enums

--- src/store/root.mutations.ts
+++ src/store/root.mutations.ts
@@ -1,14 +1,40 @@
 import { MutationTree } from "vuex";
 import { RootState } from "./root.models";
 
+enum MutationsTypes {
+  SET_LOADING = "setLoading",
+  SET_SNACKBAR = "setSnackbar",
+}
+
 const mutations: MutationTree<RootState> = {
-  setLoading: (state, { payload }: { payload: RootState["loading"] }) => {
+  [MutationsTypes.SET_LOADING]: (
+    state,
+    { payload }: { payload: RootState["loading"] },
+  ) => {
     state.loading = payload;
   },
-  showSnackbar: (state, { payload }: { payload: RootState["snackbar"] }) => {
-    state.snackbar.type = payload.type ? payload.type : "success";
-    state.snackbar.message = payload.message;
+  [MutationsTypes.SET_SNACKBAR]: (
+    state,
+    { payload }: { payload: RootState["snackbar"] },
+  ) => {
+    state.snackbar = {
+      message: payload.message,
+      type: payload.type || "success",
+      isActive: payload.isActive,
+    };
   },
+};
+
+/** Helpers types */
+export const mutationsTypes = {
+  [MutationsTypes.SET_LOADING]: (payload: RootState["loading"]) => ({
+    type: MutationsTypes.SET_LOADING,
+    payload,
+  }),
+  [MutationsTypes.SET_SNACKBAR]: (payload: RootState["snackbar"]) => ({
+    type: MutationsTypes.SET_SNACKBAR,
+    payload,
+  }),
 };
 
 export default mutations;
