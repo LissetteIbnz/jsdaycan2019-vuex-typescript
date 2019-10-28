018ea28253a5d7f1eca0c9e48d2852b184b57f2b
Sara Lissette <lissette.ibnz@gmail.com>
2019-10-28 17:04:03 +0000

    Create getters with enums

---
+++ src/store/root.getters.ts
@@ -0,0 +1,20 @@
+import { GetterTree } from "vuex";
+import { RootState } from "./root.models";
+
+enum GettersTypes {
+  SNACKBAR = "snackbar",
+}
+
+const getters: GetterTree<RootState, {}> = {
+  [GettersTypes.SNACKBAR]: (state): RootState["snackbar"] => {
+    const { isActive, message, type } = state.snackbar;
+    return { isActive, message, type };
+  },
+};
+
+/** Helpers types */
+export const gettersTypes = {
+  [GettersTypes.SNACKBAR]: GettersTypes.SNACKBAR,
+};
+
+export default getters;
